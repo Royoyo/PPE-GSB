faf44b9737a093fc4785b3652474473e1f1316cf
Guillaume Haag <haag.guillaume@gmail.com>
2015-07-20 21:13:34 +0200

    Création de la solution asp.net

---
+++ Cas GSB/Cas GSB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cas_GSB.Startup))]
+namespace Cas_GSB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
